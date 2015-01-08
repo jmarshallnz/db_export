ccef503dce79ef5f9967eca78e844b451e942ff9
Jonathan Marshall <jmarshall@never.you.mind>
2015-01-09 11:36:24 +1300

    update to read data from pubmlst etc.

--- access_export_to_csv.R
+++ access_export_to_csv.R
@@ -45,16 +45,112 @@ library(dplyr)
 
 db <- read.csv("../Export_Bionumerics_DO_NOT_CHANGE.csv", stringsAsFactors=F)
 
-# 1. Eliminate the rows we don't want.
-#   a. rows without MLST allele information
+# 1. Process MLST information.
 
-db = db %>% filter(ASP != "" |
-                   GLN != "" |
-                   GLT != "" |
-                   GLY != "" |
-                   PGM != "" |
-                   TKT != "" |
-                   UNC != "")
+cols_mlst  <- c("ASP", "GLN", "GLT", "GLY", "PGM", "TKT", "UNC")
+
+#   a. convert "NEW" to a special value?
+
+# TODO: What does NEW mean?  New at the time of sampling?  Why not in PubMLST?
+
+# Talked with Anne. NEW is new at the time of sampling.  i.e. it could be known now
+# and possibly is now in PubMLST.  Will see if I can get the sequence information that
+# corresponds to these NEW isolates.
+
+for (i in cols_mlst) {
+  newbies <- db[,i] == "NEW"
+  if (sum(newbies) > 0)
+    db[newbies,i] <- 1000 - 1:sum(newbies)
+}
+
+#   a. convert MLST info to numeric
+
+
+for (i in cols_mlst)
+  db[,i] <- as.numeric(db[,i])
+
+#   b. eliminate rows without MLST information.
+
+db = db %>% filter(!is.na(ASP) |
+                   !is.na(GLN) |
+                   !is.na(GLT) |
+                   !is.na(GLY) |
+                   !is.na(PGM) |
+                   !is.na(TKT) |
+                   !is.na(UNC))
+
+#   c. ST from PubMLST, optionally imputing missing alleles
+
+impute_alleles <- 0 # maximum number of imputed alleles
+
+# download pubmlst data
+pubmlst <- read.table("http://pubmlst.org/data/profiles/campylobacter.txt", header=T, sep="\t")
+cols_pub <- 2:8
+
+ST <- rep(NA, nrow(db))
+impute <- rep(0, impute_alleles+2)
+new_sts <- 0
+for (i in 1:nrow(db)) {
+  st <- db[i, cols_mlst]
+  possibles <- rep(T, nrow(pubmlst))
+  na_count <- 0
+  for (j in 1:length(st)) {
+    if (!is.na(st[,j])) {
+      possibles <- possibles & pubmlst[,cols_pub[j]] == st[,j]
+    } else {
+      na_count <- na_count + 1;
+    }
+  }
+  if (sum(possibles) == 0 && na_count == 0) {
+    # new ST?
+    cat("New full-profile ST found at row", i, "\n")
+    new_sts <- new_sts+1
+    pubmlst[nrow(pubmlst)+1,1:8] <- c(10000 + new_sts, st)
+  } else if (sum(possibles) == 0) {
+    cat("New partial-profile ST found with", na_count, "missing loci at row", i, "\n")
+  }
+  else if (na_count <= impute_alleles && sum(possibles) == 1) {
+    # found - fill in the gaps from PubMLST
+    ST[i] <- pubmlst[possibles, "ST"]
+    db[i, cols_mlst] <- pubmlst[possibles, cols_pub]
+    impute[na_count+1] <- impute[na_count+1] + 1
+  } else {
+    impute[impute_alleles+2] <- impute[impute_alleles+2] + 1
+  }
+  if (i %% 100 == 0)
+    cat("done", i, "of", nrow(db), "rows\n")
+}
+
+#   d. compare to ST in database.
+
+compST <- cbind(db$ST, ST)
+
+compST[db$ST != "" & is.na(ST),]
+compST[db$ST == "" & !is.na(ST),]
+compST[db$ST != ST & !is.na(ST),]
+
+db$ST = ST
+
+
+
+# 2. Eliminate the rows we don't want.
+#   a. rows in the wrong project
+
+db = db %>% filter(Project != "Els Acke Pet Food" &
+                   Project != "UKVTRI" &
+                   Project != "Abattoir Cases" &
+                   Project != "Barbara Binney" &
+                   Project != "Caecal Project" &
+                   Project != "Occupational" &
+                   Project != "UKVTRI" &
+                   Project != "Poultry Industry" &
+                   Project != "Timaru" &
+                   Project != "Tui" &
+                   Project != "Steve Trewick" &
+                   Project != "Random" &
+                   Project != "TeckLok" &
+                   Project != "Whitford Wild Bird Trust" &
+                   Project != "")
 
 #   b. rows without source information
 
@@ -63,29 +159,107 @@ db = db %>% filter(ASP != "" |
 
 db = db %>% filter(SA_model_source != "")
 
-#   c. human isolates that aren't primaries and don't have unique STs
+#   c. human isolates outside of the Manawatu
+
+# TODO: Is the Manawatu column derivable from other columns?  Can we check this?
+
+db = db %>% filter(Manawatu != "No" | SA_model_source != "Human")
+
+#   d. human isolates that aren't primaries and don't have unique STs
 
 # TODO: It's not clear what to do if Primary is not specified as Yes or No.  Some are blanks.
 #       Even if it is No, we should still include if there is another isolate ID and the ST was unique?
 
-#   d. non-Manawatu human isolates
+non_primary = db %>% filter(Source.Type == "Human" & Primary.sample == "No")
 
-# TODO: Do we want to trust the Palmerston North column here, or PN Hospital, or?
+# find corresponding primaries if they're a duplicate, and compare STs (Note: Need ST information here)
+primary = db %>% filter(Source.Type == "Human" & Primary.sample != "No")
 
-#   e. projects we wish to preclude.
+join_cols = as.vector("LabID")
+names(join_cols)
 
-# TODO: Any other rows to eliminate?
+include_np <- non_primary %>% inner_join(primary, by=c("Duplicate.to" = "Lab.ID"))
+
+include_np = include_np %>% filter(ASP.x != ASP.y |
+                                   GLN.x != GLN.y |
+                                   GLT.x != GLT.y |
+                                   GLY.x != GLY.y |
+                                   PGM.x != PGM.y |
+                                   TKT.x != TKT.y |
+                                   UNC.x != UNC.y)
+
+# add these rows in
+db = db %>% filter(Source.Type != "Human" | Primary.sample != "No" | Lab.ID %in% include_np$Lab.ID)
+
+#   e. Sources we wish to eliminate
+
+db = db %>% filter(SA_model_source != "Pig")
+
+#   f. Incomplete SA profiles
+db = db %>% filter(!is.na(ASP) &
+                   !is.na(GLN) &
+                   !is.na(GLT) &
+                   !is.na(GLY) &
+                   !is.na(PGM) &
+                   !is.na(TKT) &
+                   !is.na(UNC) & 
+                   !is.na(ST))
+
+# fixup date information
+db <- db %>% mutate(ReportDate = as.Date(ReportDate, format="%d/%m/%Y"))
+
+# eliminate those humans without a date
+db <- db %>% filter(!is.na(ReportDate) | Source.Type != "Human")
+
+# add a quarter and intervention column
+quarters_since_2005 <- function(date) {
+  dt <- as.POSIXlt(date)
+  floor(dt$mon/3) + (dt$year - 105)*4 + 1
+}
+
+db <- db %>% mutate(Quarter = quarters_since_2005(ReportDate))
+db <- db %>% mutate(Intervention = ifelse(Quarter <= 12, "before", "after"))
+
+# eliminate things outside our range
+db <- db %>% filter(Quarter > 0 | is.na(Quarter))
+
+# TODO: Get prevalence information directly from the number of isolates?
 
 
+# load concordance file to map urban/rural status
+ur <- read.csv("../concordance-2006.csv", stringsAsFactors=F)
+names(ur) <- c("Meshblock06", "UR_cat")
 
+# convert UR_cat to a number, and U/R binary
+ur_tab <- read.table(header=T, sep=",", stringsAsFactors=F, strip.white=T, text = "
+            UR_cat,                               UR_num, UR_bool      
+            Area outside urban/rural profile,         NA, NA 
+            Highly rural/remote area,                 -3, Rural
+            Rural area with low urban influence,      -2, Rural
+            Rural area with moderate urban influence, -1, Rural
+            Rural area with high urban influence,      0, Urban
+            Independent Urban Area,                    1, Urban
+            Satellite Urban Area,                      2, Urban
+            Main urban area,                           3, Urban"
+)
 
+ur = ur %>% left_join(ur_tab, by="UR_cat")
 
+table(ur$UR_cat)
+table(ur$UR_num)
+table(ur$UR_bool)
 
+# join our database
+db <- db %>% left_join(ur, by="Meshblock06")
 
-db[[col_names_mlst_alleles[1]]]
+# Eliminate columns we don't want
+sub <- db %>% select(ST, ASP, GLN, GLT, GLY, PGM, TKT, UNC, SA_model_source, UR_num, UR_bool, ReportDate, Quarter, Intervention)
 
+# write to .csv file
+write.csv(sub, "output.csv", row.names=F)
 
-# 1. Eliminate e
+# TODO: Write R Markdown document to encapsulate this information.
+
 
 # The 2013 procedure was as follows:
 #
